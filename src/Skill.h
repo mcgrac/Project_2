e7a4c52f912c201560c72bea9b856269c3a674b4
mcgrac <marcsie2503@gmail.com>
2026-04-30 12:54:13 +0200

    little changes

--- src/Skill.h
+++ src/Skill.h
@@ -44,10 +44,6 @@ public:
     inline bool GetHasAreaEffect() const { return hasAreaEffect; }
     inline bool GetAreaEffectTargetAllies() const { return areaEffectTargetAllies; }
     inline std::string GetAnimationId() const { return animationId; }
-
-    inline void SetHasAreaEffect(bool b) { hasAreaEffect = b; }
-    inline void SetAreaEffectTargetAllies(bool b) { areaEffectTargetAllies = b; }
-
     inline DamageType GetDamageType() const { return damageType; }
 
     inline std::string GetDescription() const { return skillDescription; }
@@ -58,6 +54,12 @@ public:
     //--------------------------
 #pragma endregion
 
+#pragma region SETTERS
+    inline void SetHasAreaEffect(bool b) { hasAreaEffect = b; }
+    inline void SetAreaEffectTargetAllies(bool b) { areaEffectTargetAllies = b; }
     inline void SetDescription(const std::string& desc) { skillDescription = desc; }
+#pragma endregion
+
+
 
 };
