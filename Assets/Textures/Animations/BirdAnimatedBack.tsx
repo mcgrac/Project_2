b0621375edf8c71e1bf326e82b287adbe72cfa51
Orlock6400 <bernat.pazos@estudiantat.upc.edu>
2026-05-14 10:44:59 +0200

    fullBack

---
+++ Assets/Textures/Animations/BirdAnimatedBack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="BirdAnimatedBack" tilewidth="1280" tileheight="720" tilecount="11" columns="1">
+ <image source="BirdAnimatedBack.png" width="1280" height="7920"/>
+</tileset>
